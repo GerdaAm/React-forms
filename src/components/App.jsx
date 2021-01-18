cb218720c1b973920f9fe03b8bd7bf8cb2984905
GerdaAm <63974776+GerdaAm@users.noreply.github.com>
2021-01-18 07:52:00 +0200

    Event handler using event properties to track changes in form. button in form - event.preventDefault

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [isNameSubmitted, setNameSubmitted] = useState(false);
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleClick(event) {
+    setNameSubmitted(true);
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {isNameSubmitted && name}</h1>
+      <form onSubmit={handleClick}>
+        <input
+          type="text"
+          placeholder="What's your name?"
+          onChange={handleChange}
+          value={name}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
